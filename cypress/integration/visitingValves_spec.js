1d4c3205eb863927ff6455f4d12eec8dbaab3378
LucyMHall <lucymhall1995@gmail.com>
2019-09-04 15:19:31 +0100

    Test: alter feature test of /valve to check for header and paragraph separately

--- cypress/integration/visitingValves_spec.js
+++ cypress/integration/visitingValves_spec.js
@@ -8,8 +8,15 @@ describe('visitingValves', function () {
     cy.get('title').should('have.text', 'Welcome to Acme');
   });
 
-  it('displays the contents of the correct markdown file in the body', function() {
+  it('displays the header of correct markdown file in the body', function() {
     cy.visit('http://localhost:5000/valves');
-    cy.get('body').should('have.text', "Valves\nAcme Co. valves are amongst the highest quality in the industry. Whether it's for industrial, commercial, medical or space exploration, you can always count on an Acme Co. valve.");
+    const mdHeaderForValves = 'Valves'
+    cy.get('h1').contains(mdHeaderForValves)
+  });
+
+  it('displays the paragraph of correct markdown file in the body', function() {
+    cy.visit('http://localhost:5000/valves');
+    const mdParaForValves = "Acme Co. valves are amongst the highest quality in the industry. Whether it's for industrial, commercial, medical or space exploration, you can always count on an Acme Co. valve."
+    cy.get('p').contains(mdParaForValves)
   });
 });
