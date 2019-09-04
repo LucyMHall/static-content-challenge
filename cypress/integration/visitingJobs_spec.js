692cf84328e1841959fd69e891fddbcc9e6d1862
LucyMHall <lucymhall1995@gmail.com>
2019-09-04 15:25:00 +0100

    Test: add in feature test for /jobs to check for correct markdown content

--- cypress/integration/visitingJobs_spec.js
+++ cypress/integration/visitingJobs_spec.js
@@ -9,9 +9,18 @@ describe('visitingJobsPage', function () {
     cy.get('title').should('have.text', 'Welcome to Acme');
   });
 
-  it('displays the placeholder text The Jobs Page when jobs is visited', function() {
-    cy.visit('http://localhost:5000/jobs')
-    cy.get('h1').should('have.text', 'The Jobs Page');
+  it('displays the header of correct markdown file in the body', function() {
+    cy.visit('http://localhost:5000/jobs');
+    const mdHeaderForValves = 'Jobs at Acme Co.'
+    cy.get('h1').contains(mdHeaderForValves)
+  });
+
+  it('displays the paragraph of correct markdown file in the body', function() {
+    cy.visit('http://localhost:5000/jobs');
+    const mdPara1Jobs = "Acme Co. is often seeking candidates in the areas of marketing, finance, customer support and sales. If you are an enthusiastic go-getter, you don't need to look any further for your next step up the corporate career ladder.";
+    const mdPara2Jobs = "Send us an email at careers@acme.com for more information."
+    cy.get('p').first().contains(mdPara1Jobs);
+    cy.get('p').last().contains(mdPara2Jobs);
   });
 
 });
